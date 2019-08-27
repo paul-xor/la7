c24a91dd568b79d4a92ee41d6cc0664b91106a3a
Pasha Smolov <palexsmol@gmail.com>
2019-08-27 11:23:09 -0700

    add view partials and refactor

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const router = express.Router();
 router.get("/", function(req, res) {
   res.render("index.ejs");
 });
+router.get("/about", function(req, res) {
+  res.render("about.ejs");
+});
 
 // MIDDLEWARE.
 app.set("view engine", "ejs");
